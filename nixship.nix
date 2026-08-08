ae6831a18056a3a92bf6523bfe6f08c0af28f5fd
XADE <notadevel@gmail.com>
2026-08-08 18:43:47 +0530

    Patch audited nanoid dependency

--- nixship.nix
+++ nixship.nix
@@ -9,7 +9,7 @@ let
     version = "0.1.0";
     src = self;
     pnpm = pkgs.pnpm_10;
-    hash = "sha256-vLYeddSLBZ3WUe/bmc/lPMGFmqWrza4UYGDmuLEmrTk=";
+    hash = "sha256-oF7t3eJw7uhA76TwraVa+fYfvL7sDD9Yd5VhGqqZ2yQ=";
     fetcherVersion = 3;
   };
 in
